d330b4d19cb324fd93f60b46d803aacfe66dc7b7
하송희 <99heesong@naver.com>
2023-09-01 09:15:39 +0900

    230901 알고리즘

---
+++ programmers/pg_lv1_카드뭉치.js
@@ -0,0 +1,8 @@
+function solution(cards1, cards2, goal) {
+  for (i = 0; i < goal.length; i++) {
+    if (cards1[0] == goal[i]) cards1.splice(0, 1);
+    else if (cards2[0] == goal[i]) cards2.splice(0, 1);
+    else return "No";
+  }
+  return "Yes";
+}
