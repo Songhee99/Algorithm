ea2b88a06792b7a7611979069a6b94712a9fd98f
하송희 <99heesong@naver.com>
2024-06-19 03:51:56 +0900

    240619 알고리즘

--- python/Baekjoon/16401.py
+++ python/Baekjoon/16401.py
@@ -3,15 +3,11 @@ import sys
 n, m = map(int, sys.stdin.readline().rstrip().split())
 snacks = list(map(int, sys.stdin.readline().rstrip().split()))
 
-start = 1
-end = max(snacks)
+start, end = 1, max(snacks)
 
 while start <= end:
     mid = (start + end) // 2
-    count = 0
-
-    for i in snacks:
-        count += i // mid
+    count = sum([(i // mid) for i in snacks])
 
     if count < n: end = mid - 1
     else: start = mid + 1
