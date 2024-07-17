fb9e8113a2efe18e106034921e133c421580eb51
하송희 <99heesong@naver.com>
2024-07-18 04:07:39 +0900

    240718 알고리즘

--- Baekjoon/19637.js
+++ Baekjoon/19637.js
@@ -1,19 +1,24 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [num, ...character] = fs.readFileSync(filePath).toString().trim().split("\n");
-const [n, m] = num.split(" ");
-const title = character.splice(0, +n).map((v, i) => v.split(" "));
-let answer = [];
-let caseNum = 0;
+const [nm, ...power] = fs.readFileSync(filePath).toString().trim().split("\n");
+const [n, m] = nm.split(" ").map(Number);
+const titleArr = power.splice(0, n).map((v) => v.split(" "));
+const result = [];
 
-for (item of character) {
-  while (true) {
-    if (+item <= +title[caseNum][1]) {
-      answer.push(title[caseNum][0]);
-      break;
-    }
-    caseNum++;
+power.forEach((ele) => {
+  if (+ele <= +titleArr[0][1]) return result.push(titleArr[0][0]);
+  else if (+ele > +titleArr[n - 1][1]) return result.push(titleArr[n - 1][0]);
+
+  let start = 0;
+  let end = n - 1;
+
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+
+    if (+ele <= +titleArr[mid][1]) end = mid - 1;
+    else start = mid + 1;
   }
-}
+  result.push(titleArr[start][0]);
+});
 
-console.log(answer.join("\n"));
+console.log(result.join("\n"));
