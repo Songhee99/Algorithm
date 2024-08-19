561cd71265637efcb67a577f7ea443da699430f2
하송희 <99heesong@naver.com>
2024-08-19 13:33:24 +0900

    240819 알고리즘

--- Baekjoon/11866.js
+++ Baekjoon/11866.js
@@ -1,19 +1,4 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [n, k] = fs.readFileSync(filePath).toString().trim().split(" ");
-let result = [];
-let queue = [];
-let idx = 0;
-
-for (i = 1; i < +n + 1; i++) {
-  queue.push(i);
-}
-
-while (result.length < +n) {
-  for (j = 0; j < +k - 1; j++) {
-    queue.push(queue[idx++]);
-  }
-  result.push(queue[idx++]);
-}
-
-console.log("<" + result.join(", ") + ">");
+const [nm, ...abs] = fs.readFileSync(filePath).toString().trim().split(" ");
+const [n, m] = nm.split(" ").map(Number);
