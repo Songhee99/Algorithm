16dddd502c4e8b37407762bd892a86cf43d20ddb
하송희 <99heesong@naver.com>
2024-11-16 21:07:47 +0900

    241116 알고리즘

--- Baekjoon/11724.js
+++ Baekjoon/11724.js
@@ -1,27 +1,27 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [nm, ...maps] = fs.readFileSync(filePath).toString().trim().split("\n");
-const [n, m] = nm.split(" ");
-const graph = Array.from({ length: +n + 1 }, () => []);
-maps.forEach((ele) => {
-  const [a, b] = ele.split(" ");
-  graph[+a].push(+b);
-  graph[+b].push(+a);
+const [nm, ...uvs] = fs.readFileSync(filePath).toString().trim().split("\n");
+const [n, m] = nm.split(" ").map(Number);
+const graph = Array.from({ length: n + 1 }, () => []);
+uvs.forEach((ele) => {
+  const [u, v] = ele.split(" ").map(Number);
+  graph[u].push(v);
+  graph[v].push(u);
 });
-const visited = Array.from({ length: +n + 1 }, () => false);
+const visited = Array.from({ length: n + 1 }, () => false);
 let count = 0;
 
-const dfs = (graph, v, visited) => {
-  visited[v] = true;
-  graph[v].forEach((v) => {
-    if (visited[v] == false) dfs(graph, v, visited);
+const dfs = (i) => {
+  visited[i] = true;
+  graph[i].forEach((ele) => {
+    if (!visited[ele]) dfs(ele);
   });
 };
 
-for (i = 1; i < visited.length; i++) {
-  if (visited[i] == false) {
+for (let i = 1; i <= n; i++) {
+  if (!visited[i]) {
+    dfs(i);
     count++;
-    dfs(graph, i, visited);
   }
 }
 
