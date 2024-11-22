e905e1fe93945154ed46f10d9d989e589e57e5fd
하송희 <99heesong@naver.com>
2024-11-22 12:27:38 +0900

    241122 알고리즘

--- Baekjoon/2630.js
+++ Baekjoon/2630.js
@@ -1,39 +1,32 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const [n, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
-const graph = [];
-input.forEach((ele) => {
-  graph.push(ele.split(" "));
-});
-let white = 0;
-let blue = 0;
+const paper = input.map((v) => v.split(" ").map(Number));
+let colorsCount = [0, 0];
 
 const checkColor = (y, x, size) => {
-  const curColor = graph[y][x];
-  let same = true;
+  const curColor = paper[y][x];
+  let flag = true;
 
-  for (i = y; i < y + size; i++) {
-    for (j = x; j < x + size; j++) {
-      if (graph[i][j] != curColor) {
-        same = false;
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if (paper[y + i][x + j] != curColor) {
+        flag = false;
         break;
       }
     }
-    if (!same) break;
+    if (!flag) break;
   }
 
-  if (same) {
-    if (curColor == "0") white++;
-    else blue++;
-  } else {
-    const newSize = size / 2;
-    checkColor(y, x, newSize);
-    checkColor(y + newSize, x, newSize);
-    checkColor(y, x + newSize, newSize);
-    checkColor(y + newSize, x + newSize, newSize);
+  if (flag) colorsCount[curColor]++;
+  else {
+    const tmpSize = size / 2;
+    checkColor(y, x, tmpSize);
+    checkColor(y + tmpSize, x, tmpSize);
+    checkColor(y, x + tmpSize, tmpSize);
+    checkColor(y + tmpSize, x + tmpSize, tmpSize);
   }
 };
 
-checkColor(0, 0, n);
-
-console.log(white + "\n" + blue);
+checkColor(0, 0, +n);
+console.log(colorsCount.join("\n"));
