18c39e73c0a78876363d8ccc32e444d59c09525a
하송희 <99heesong@naver.com>
2024-11-25 11:28:41 +0900

    241125 알고리즘

--- Baekjoon/2579.js
+++ Baekjoon/2579.js
@@ -1,21 +1,25 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [n, ...stairs] = fs.readFileSync(filePath).toString().trim().split("\n");
+const [n, ...stairs] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
 
 if (n == 1) return console.log(stairs[0]);
-if (n == 2) return console.log(+stairs[0] + +stairs[1]);
+if (n == 2) return console.log(stairs[0] + stairs[1]);
 
-const stairsSum = new Array(+n).fill(0);
+let numsSum = Array.from({ length: n }, () => 0);
+numsSum[0] = stairs[0];
+numsSum[1] = stairs[0] + stairs[1];
+numsSum[2] = Math.max(stairs[0] + stairs[2], stairs[1] + stairs[2]);
 
-stairsSum[0] = +stairs[0];
-stairsSum[1] = +stairs[0] + +stairs[1];
-stairsSum[2] = Math.max(+stairs[0] + +stairs[2], +stairs[1] + +stairs[2]);
-
-for (i = 3; i < +n; i++) {
-  stairsSum[i] = Math.max(
-    +stairs[i] + stairsSum[i - 2],
-    +stairs[i] + +stairs[i - 1] + stairsSum[i - 3]
+for (let i = 3; i < n; i++) {
+  numsSum[i] = Math.max(
+    numsSum[i - 2] + stairs[i],
+    numsSum[i - 3] + stairs[i - 1] + stairs[i]
   );
 }
 
-console.log(stairsSum[n - 1]);
+console.log(numsSum[n - 1]);
