9c76dc8f999f3585a2de2e399b0e0a7c95d6f229
하송희 <99heesong@naver.com>
2024-12-28 19:21:50 +0900

    241228 알고리즘

--- python/Baekjoon/1389.py
+++ python/Baekjoon/1389.py
@@ -1,36 +1,23 @@
 import sys
-from collections import deque
 
 n, m = list(map(int, sys.stdin.readline().strip().split()))
-sumList = [0 for _ in range(n + 1)]
-graph = [[] for _ in range(n + 1)]
+graph = [[float("inf")] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    graph[i][i] = 0
 for _ in range(m):
     a, b = list(map(int, sys.stdin.readline().strip().split()))
-    graph[a].append(b)
-    graph[b].append(a)
+    graph[a][b] = 1
+    graph[b][a] = 1
 
-def bfs(i, j):
-    queue = deque([(i, 0)])
-    visited = [False] * (n + 1)
-    visited[i] = True
-
-    while queue:
-        num, count = queue.popleft()
-        if num == j: return count
-
-        for k in graph[num]:
-            if not visited[k]:
-                visited[k] = True
-                queue.append((k, count + 1))
-
-for i in range(1, n):
-    for j in range(i + 1, n + 1):
-        count = bfs(i, j)
-        sumList[i] += count
-        sumList[j] += count
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(i + 1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+            graph[j][i] = graph[i][j]
 
 result = [0, float("inf")]
-for (idx, curSum) in enumerate(sumList[1:]):
-    if curSum < result[1]: result = [idx, curSum]
+for i in range(1, n + 1):
+    curSum = sum(graph[i][1:])
+    if curSum < result[1]: result = [i, curSum]
 
-print(result[0] + 1)
+print(result[0])
