6e718b94c5cbeb2a1e9c3a0c00b5cfbb9b7d64a3
하송희 <99heesong@naver.com>
2025-04-06 19:29:39 +0900

    250406 알고리즘

--- python/Baekjoon/17073.py
+++ python/Baekjoon/17073.py
@@ -1,35 +1,13 @@
 import sys
-sys.setrecursionlimit(10**6)
 
 n, w = map(int, sys.stdin.readline().strip().split())
-graph = [[] for _ in range(n + 1)]
+graph = [0] * (n + 1)
 for _ in range(n - 1):
     u, v = map(int, sys.stdin.readline().strip().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    graph[u] += 1
+    graph[v] += 1
 
-visited = [False] * (n + 1)
-total = 0
-count = 0
+graph[1] = 0
+leaf = graph.count(1)
 
-def dfs(i, water):
-    global count
-    global total
-    visited[i] = True
-    tmp = []
-
-    for j in graph[i]:
-        if not visited[j]: tmp.append(j)
-
-    if len(tmp) == 0:
-        count += 1
-        total += water
-        return
-    else:
-        nextWater = water / len(tmp)
-        for k in tmp:
-            dfs(k, nextWater)
-
-dfs(1, w)
-
-print(total / count)
+print(w / leaf)
